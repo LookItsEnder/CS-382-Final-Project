d04e8ef8cd5edcdfd6815c318638c95500e2f7f3
Ender <131053595+LookItsEnder@users.noreply.github.com>
2024-12-03 19:56:09 -0600

    Missed one.
    This file still has mentions to the contact page, which doesn't exist.

--- CS-382-Final-Project-Zach/php files/register.php
+++ CS-382-Final-Project-Zach/php files/register.php
@@ -53,7 +53,6 @@ if ($_SERVER ['REQUEST_METHOD'] === 'POST') {
    <a href="../WebPages/index.php">Home</a>
    <a href="../WebPages/canvas.php">Canvas</a>
    <a href="../WebPages/Gallery.php">Gallery</a>
-   <a href="#">Contact</a>
    
  
 
